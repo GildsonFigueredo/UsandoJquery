0d3c22482c6ced2aa2600731448134d2e181f00e
GildsonFigueredo <122454886+GildsonFigueredo@users.noreply.github.com>
2025-05-14 18:48:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function copiaTexto( ) {
     var x = $("#c").val(); // Acessa o valor do elemento '#c'
     $("#d").html(x);       // Atribui novo conteúdo para elemento '#d'
 }
+//Bloco de função em JavaScript
+//function copiaTexto( ) {
+//    var x = document.getElementById("c").value;
+//    document.getElementById("d").innerHTML = x;
+//}
 
 function mudaCor( ){        
     $('#d').css('background-color', 'blue'); // Atribui cor de fundo  
 }
+//Bloco de função em JavaScript
+//function mudaCor( ){	
+//    document.getElementById('#d').style.color = 'blue';
+// }
+
